876331b64e1fbe3803e0211c88d38baf78865f0d
chiriane <164158854+chiriane@users.noreply.github.com>
2024-03-21 00:24:24 -1200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+function Open() {
+    document.getElementById('myImage').src = 'openeyes.gif';
+    document.getElementById('Description').style.display = 'block';
+}
+function Close() {
+    document.getElementById('myImage').src = 'closeeyes.png';
+    document.getElementById('Description').style.display = 'None';
+}
